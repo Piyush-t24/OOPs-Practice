ffec392ea99bf5b9ff415f7aea9535286a22f15a
Piyush-t24 <piyushiitian2023@gmail.com>
2024-11-20 14:47:25 +0530

    Way of Inheritence

--- Extra Questions/WayOfInheritence.cpp
+++ Extra Questions/WayOfInheritence.cpp
@@ -1,4 +1,4 @@
-class Parent
+/*class Parent
 {
 private: int a;
 protected: int b;
@@ -38,4 +38,91 @@ int main()
 //  c.a=10;
 //  c.b=5;
  c.c=20;
+}*/
+
+
+/*class Parent
+{
+private: int a;
+protected: int b;
+public: int c;
+
+ void funParent()
+ {
+ a=10;
+ b=5;
+ c=15;
+ }
+};
+class Child: protected Parent
+{
+public:
+
+ void funChild()
+ {
+//  a=10;
+ b=5;
+ c=15;
+ }
+};
+class GrandChild : public Child
+{
+public:
+ void funGrandChild()
+ {
+//  a=10;
+ b=5;
+ c=20;
+ }
+};
+int main()
+{
+ Child c;
+//  c.a=10;
+//  c.b=5;
+//  c.c=20;
+}*/
+
+
+
+class Parent
+{
+private: int a;
+protected: int b;
+public: int c;
+
+ void funParent()
+ {
+ a=10;
+ b=5;
+ c=15;
+ }
+};
+class Child: private Parent
+{
+public:
+
+ void funChild()
+ {
+//  a=10;
+ b=5;
+ c=15;
+ }
+};
+class GrandChild : public Child
+{
+public:
+ void funGrandChild()
+ {
+//  a=10;
+//  b=5;
+//  c=20;
+ }
+};
+int main()
+{
+ Child c;
+//  c.a=10;
+//  c.b=5;
+//  c.c=20;
 }
