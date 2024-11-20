877a757f5ae3dfa07111e0c3b38179cf6db6c6a4
Piyush-t24 <piyushiitian2023@gmail.com>
2024-11-20 16:25:30 +0530

    Base Class Pointe

---
+++ Extra Questions/BaseClassPointerDerivedClassObject.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Base 
+{
+public:
+    void fun1()
+    {
+        cout<<"fun1 of Base"<<endl;
+    }
+};
+
+class Derived:public Base
+{
+public:
+    void fun2()
+    {
+        cout<<"fun2 of Derived"<<endl;
+    }
+};
+
+/*
+int main()
+{
+    Derived d;
+    d.fun1();
+    d.fun2();
+    return 0;
+}
+*/
+
+
+// OR
+
+int main()
+{
+    Derived d;
+    Base *ptr=&d;
+    ptr-> fun1();
+    // ptr-> fun2();
+    return 0;
+}
